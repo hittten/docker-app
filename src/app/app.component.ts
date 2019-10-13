ef3644c5b2478bdbc53a29326e48f86b19a3a817
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2019-10-13 19:23:37 +0200

    add hello service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'docker-app';
+  message$ = this.http.get<{ message: string }>(environment.apiUrl + '/hello/');
+
+  constructor(private http: HttpClient) {
+  }
 }
